159d0b57f19a61ab630bd813bd3e55eb305136cf
anxiouscrypt <anxiouscrypt@gmail.com>
2021-04-17 00:56:34 +0300

    Version 2.0

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/env pyhton3 
 import instaloader
 import argparse
 import getpass
 import time
 
+
+
+# Parse Arguments
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--account", required=True,
+  help="name of the account to be scraped for followers")
+
+ap.add_argument("-s", "--followers", required=False,
+  help="print the account's followers list", action="store_true")
+
+ap.add_argument("-g", "--following", required=False,
+  help="print the account's following list", action="store_true")
+
+
+args = vars(ap.parse_args())
+
+
+# Set Variables
+
+targetAccount = ""
+
+
+# Print welcome statment
 
 print('''
 
@@ -18,27 +41,18 @@ print('''
 ------ ------  |      |  |
 
 
-		Version: 1.1
-		@anxiouscrypt
+    Version: 2.0
+    @anxiouscrypt
 
-	''')
+  ''')
 
 
-# parse arguments and get account name and percentage of followers to be extracted
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-a", "--account", required=True,
-	help="name of the account to be scraped for followers")
-
-args = vars(ap.parse_args())
 
 targetAccount = ""
-login_username = ""
-login_password = ""
-
 
 if args["account"]:
-	targetAccount = format(args["account"])
+  targetAccount = format(args["account"])
+
 
 
 # Get Login Credentials
@@ -47,11 +61,12 @@ login_username = input("[WELCOME]: Enter the Username you would like to login wi
 time.sleep(2)
 login_password = getpass.getpass("[SECURE]: Enter your Password:")
 
+
 # Login or load session
 
 L = instaloader.Instaloader()
 print('[INFO]: Logging in..')
-L.login(login_username, login_password)        # (login)
+L.login(login_username, login_password)        # (Regular login)
 print('[INFO]: Logged in')
 time.sleep(2)
 
@@ -61,62 +76,45 @@ time.sleep(2)
 profile = instaloader.Profile.from_username(L.context, targetAccount)
 
 
-# Print list of followees
+# Print list of followers
 
 print("[INFO]: Extracting followers' list from " + targetAccount + '...')
 
-follow_list = []
-
-file = open(targetAccount + "_followers.txt","w")
-
-count=0
-
+followers_list = []
 for followee in profile.get_followers():
-    follow_list.append(followee.username)
-    file.write(follow_list[count])
-    file.write("\n")
-    count=count+1
-file.close()
+    followers_list.append(followee.username)
 
-print('[INFO]: Followers list successfully saved to ' + targetAccount + '_followers.txt')
+if args["followers"]:
+  followers_file = open(targetAccount + "_followers.txt", "w")
+  for usr in followers_list:
+    followers_file.write(usr + '\n')
+  followers_file.close()
+  print('[INFO]: Followers list successfully saved to ' + targetAccount + '_followers.txt')
 
 
 # Print list of followees
 
 print("[INFO]: Extracting following's list from " + targetAccount + '...')
+
 following_list = []
-
-file = open(targetAccount + "_following.txt","w")
-
-count=0
-
 for following in profile.get_followees():
     following_list.append(following.username)
-    file.write(following_list[count])
-    file.write("\n")
-    count=count+1
-file.close()
 
-print('[INFO]: Following list successfully saved to ' + targetAccount + '_following.txt')
+if args["following"]:
+  time.sleep(1)
+  following_file = open(targetAccount + '_following.txt', "w")
+  for usr in following_list:
+    following_file.write(usr + '\n')
+  following_file.close()
+  print('[INFO]: Followers list successfully saved to ' + targetAccount + '_followers.txt')
 
 
-# Extract list of accounts that follow but dont follow back
+
 
 print('[INFO]: Extracting accounts...')
 
 time.sleep(2)
-followers_input_file = targetAccount + "_followers.txt"
-following_input_file = targetAccount + "_following.txt"
-
-Following_File = open(following_input_file, "r")
-Following = [(line.strip()) for line in Following_File]
-Following_File.close()
-
-Followers_File = open(followers_input_file, "r")
-Followers = [(line.strip()) for line in Followers_File]
-Followers_File.close()
-
-result = set(Following).difference(Followers)
+result = set(following_list).difference(followers_list)
 
 
 # Print that List
@@ -126,9 +124,11 @@ print('[INFO]: Saving to ' + targetAccount + '_results.txt...')
 File = open(targetAccount + "_results.txt", "w")
 
 for name in result:
-	File.write(name + '\n')
+  File.write(name + '\n')
 
 File.close()
 time.sleep(2)
 
 print('[INFO]: List successfully saved to ' + targetAccount + '_results.txt')
+
+# END
